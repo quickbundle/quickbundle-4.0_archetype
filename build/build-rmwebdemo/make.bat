31f6f6e1bb59d6a1892d1fc04beeb0477b188bab
Mike Bai <lostitle@gmail.com>
2013-09-16 01:11:24 +0800

    build rmwebdemo

--- build/build-rmwebdemo/make.bat
+++ build/build-rmwebdemo/make.bat
@@ -1,23 +1,29 @@
 @ECHO OFF
 cd target 
-rem copy build-rmwebdemo-4.0.0.jar...
+echo copy build-rmwebdemo-4.0.0.jar...
 rem copy build-rmwebdemo-4.0.0.jar build-rmwebdemo-4.0.0-bin\build-rmwebdemo-4.0.0
 cd build-rmwebdemo-4.0.0-bin\build-rmwebdemo-4.0.0
 rem call :extractJar build-rmwebdemo-4.0.0
 
-rem copy quickbundle-rmwebdemo...
-xcopy /e/y/i ..\..\..\..\..\quickbundle-rmwebdemo quickbundle-rmwebdemo
+echo copy quickbundle-rmwebdemo...
+rem call xcopy /e/y/i/q ..\..\..\..\..\quickbundle-rmwebdemo quickbundle-rmwebdemo
+cd ..\..\..\..\..\
+call jar cfM build/build-rmwebdemo/target/build-rmwebdemo-4.0.0-bin/build-rmwebdemo-4.0.0/quickbundle-rmwebdemo.jar quickbundle-rmwebdemo
+cd build\build-rmwebdemo\target\build-rmwebdemo-4.0.0-bin\build-rmwebdemo-4.0.0
+call jar xfM ./quickbundle-rmwebdemo.jar
+call del quickbundle-rmwebdemo.jar
+
 cd quickbundle-rmwebdemo
 call mvn clean
 cd ..
 
-rem extract plugin jars...
+echo extract plugin jars...
 call :extractJar org.quickbundle.mda.gc-4.0.0
 call :extractJar org.quickbundle.mda.gp-4.0.0
 call :extractJar org.quickbundle.mda.libs-4.0.0
 call :extractJar org.quickbundle.mda.mvm-4.0.0
 
-rem move quickbundle-rmwebdemo...
+echo move quickbundle-rmwebdemo...
 move quickbundle-rmwebdemo org.quickbundle.mda.gp-4.0.0\t\j1
 
 cd ..\..\
