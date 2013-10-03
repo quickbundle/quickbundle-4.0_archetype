db7d26918662b9e4abb5c6b5e9f162d11a012fc8
Mike Bai <lostitle@gmail.com>
2013-10-04 04:38:42 +0800

    remove json-lib

--- quickbundle-rmwebdemo/src/main/java/org/quickbundle/third/quartz/util/SchedulerHelper.java
+++ quickbundle-rmwebdemo/src/main/java/org/quickbundle/third/quartz/util/SchedulerHelper.java
@@ -1,35 +1,50 @@
 package org.quickbundle.third.quartz.util;
 
-import java.util.Iterator;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 import org.quartz.JobDataMap;
-import org.quickbundle.third.quartz.jobdetail.vo.JobDetailVo;
+import org.quickbundle.project.serializer.RmObjectMapper;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 public class SchedulerHelper implements ISchedulerConstants {
 	/**
 	 * 解析json格式的params参数
 	 * @param vo 注入params的Vo
 	 * @param request 获取json数据的request
+	 * @throws IOException 
+	 * @throws JsonMappingException 
+	 * @throws JsonParseException 
 	 */
-	public static JobDataMap parseDataMap(HttpServletRequest request) {		
+	public static JobDataMap parseDataMap(HttpServletRequest request) throws JsonParseException, JsonMappingException, IOException {		
 		String params=request.getParameter(REQ_DATA_MAP);
 		if(params == null)
 			return null;
-		JobDataMap dataMap=new JobDataMap();
+		return parseJson(params);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	static JobDataMap parseJson(String json) throws JsonParseException, JsonMappingException, IOException {
+		JobDataMap dataMap = new JobDataMap();
 		//转换json为map
-		JSONArray paramsJo=(JSONArray)JSONObject.fromObject(params).get("params");
-		Iterator itParams = paramsJo.iterator();
-		while(itParams.hasNext()){
-			JSONObject jo = (JSONObject)itParams.next();
-			if(!"".equals(jo.get("name"))) {
-				dataMap.put(String.valueOf(jo.get("name")), String.valueOf(jo.get("des")));
+		Map map = RmObjectMapper.getInstance().readValue(json, HashMap.class);
+		List<Map<String, String>> paramsJo = (List<Map<String, String>>)map.get("params");
+		for(Map keyValue : paramsJo) {
+			if(!"".equals(keyValue.get("name"))) {
+				dataMap.put(String.valueOf(keyValue.get("name")), String.valueOf(keyValue.get("des")));
+				System.out.println(String.valueOf(keyValue.get("name")) + "\t" + String.valueOf(keyValue.get("des")));
 			}
 		}
 		return dataMap;
+	}
+	
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+		System.out.println(parseJson("{\"params\":[{\"name\":\"aa\", \"des\":\"1\"}, {\"name\":\"bb\", \"des\":\"22\"}]}"));
 	}
 }
